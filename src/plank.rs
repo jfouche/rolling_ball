e3666fed27da9b89d46156e9da79db4a3d5e4f84
Jérémie Fouché <jeremie.fouche@gmail.com>
2023-08-29 19:06:49 +0200

    added plank movement

--- src/plank.rs
+++ src/plank.rs
@@ -1,13 +1,17 @@
-use bevy::prelude::*;
+use bevy::{input::mouse::MouseMotion, prelude::*};
 use bevy_rapier3d::prelude::*;
 
 pub struct PlankPlugin;
 
 impl Plugin for PlankPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_plank);
+        app.add_systems(Startup, spawn_plank)
+            .add_systems(Update, move_plank);
     }
 }
+
+#[derive(Component)]
+pub struct Plank;
 
 fn spawn_plank(
     mut commands: Commands,
@@ -19,6 +23,7 @@ fn spawn_plank(
     commands
         .spawn((
             Name::new("Plank"),
+            Plank,
             PbrBundle {
                 mesh: meshes.add(shape::Box::new(SIZE.0, SIZE.1, SIZE.2).into()),
                 material: materials.add(Color::MAROON.into()),
@@ -29,4 +34,22 @@ fn spawn_plank(
             RigidBody::Fixed,
             Collider::cuboid(SIZE.0 / 2.0, SIZE.1 / 2.0, SIZE.2 / 2.0),
         ));
+}
+
+fn move_plank(
+    mut planks: Query<&mut Transform, With<Plank>>,
+    buttons: Res<Input<MouseButton>>,
+    mut motion: EventReader<MouseMotion>,
+) {
+    if buttons.pressed(MouseButton::Right) {
+        if let Ok(mut transform) = planks.get_single_mut() {
+            for ev in motion.iter() {
+                // angle = delta px deg converted to rad
+                let dz = ev.delta.x.to_radians();
+                let dx = -ev.delta.y.to_radians();
+                transform.rotate_local_x(dx);
+                transform.rotate_local_z(dz);
+            }
+        }
+    }
 }
